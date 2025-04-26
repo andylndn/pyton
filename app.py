a95f571054448fea963479bc4edfb4c2d7cc6aba
andylndn <166704445+andylndn@users.noreply.github.com>
2025-04-26 23:34:52 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Привет! Сервер работает 🚀'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
